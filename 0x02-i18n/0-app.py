5ee9a602707d443d37657f25889844ad68a17dcf
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-05-21 05:08:45 +0100

    Render html file

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module setups a basic Flask app and creates route which renders
+html file to display an output as page.
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Renders html page."""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
