830f2919c4110743c9da4259802f4f3bd6e724cc
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-05-22 02:52:01 +0100

    Get local from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -5,28 +5,28 @@ and French stored and set default locale to English.
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
-from typing import List, Optional
 
 
 class Config:
     """Configures languages for English and French."""
-    LANGUAGES: List[str] = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE: str = "en"
-    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app: Flask = Flask(__name__)
+# Instantiate the flask app object
+app = Flask(__name__)
 app.config.from_object(Config)
 
-babel: Babel = Babel(app)
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> Optional[str]:
+def get_locale() -> str:
     """Returns best match for language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     """Route handler for the home page."""
     return render_template("2-index.html")
