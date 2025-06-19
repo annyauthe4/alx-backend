953f71faeccfbf50f4cf8c4f28e1748813f7cb75
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-19 02:49:14 +0100

    Create job queue

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,23 +2,31 @@ const kue = require('kue');
 
 const queue = kue.createQueue();
 
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  )
+}
 // Create an object for job data
 const jobData = {
- phoneNumber: '1234567890',
- message: 'A test notification',
+ phoneNumber: '4153518780',
+ message: 'This is the code to verify your account',
 }
 
 // Create job in the push_notifify
 const job = queue.create('push_notification_code', jobData)
   .save((err) => {
     if (!err) {
-       console.log(`Notification job create: ${job.id}`);
+      console.log(`Notification job created: ${job.id}`);
     }
 });
 
-// Job event listeners
-job.on('complete', () => {
-  console.log('Notification job completed');
+// Process jobs
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = jobData;
+  sendNotification(phoneNumber, message);
+  done();
 });
 
 job.on('failed', () => {
